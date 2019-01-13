2a553a5a5c9bde85f8d5ba358b85c20a4fb942fa
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2019-01-13 11:02:08 -0500

    d

--- code/logistic_regression-synthetic/test.m
+++ code/logistic_regression-synthetic/test.m
@@ -1,17 +1,21 @@
 rng('default');
 
-nn = 20000;
-d = 20;
-n = 5; % # of nodes
+nn = 100000;
+d = 100;
+n = 20; % # of nodes
 
 
 %hyper-parameter
 T=nn/n;
-eta = 1e-2;
-lambda = 1e-1;
+eta = 1e-3;
+eta_opt_basic = eta*5;
+eta_opt_1 = eta*5;
+eta_opt_2 = eta*5;
+eta_opt_3 = eta*5;
+
 beta1 = 0.9;%varying beta1 0.9 0.8 0.7
-beta2 = 0.8;
-beta3 = 0.7;
+beta2 = 0.80;
+beta3 = 0.70;
 %M = 10; %dynamics
 
 
@@ -19,52 +23,19 @@ beta3 = 0.7;
 A = zeros(d,nn);
 y = sign(rand(nn,1)-0.5);
 Xi = zeros(d,nn);
-for i=1:nn
-    if y(i,:) == 1
-        A(:,i) = 1 + randn(d,1);
-    else
-        A(:,i) = -1 + randn(d,1);
+for i=1:nn/n
+    for j=1:n
+        if y((i-1)*n+j,:) == 1
+            A(:,(i-1)*n+j) = sin(i)/10 + randn(d,1);
+        else
+            A(:,(i-1)*n+j) = -sin(i)/10 + randn(d,1);
+        end
+        Xi(:,(i-1)*n+j) = normrnd(0, (cos(t)+1)/10,d,1);
     end
-    Xi(:,i) = sin(i)*rand(d,1);
+    
+    
 end
 
-%compute the optimal reference point
-%for basic
-nn_test = 100;
-cvx_begin quiet
-variable x_ast(d,1)
-cumu_obj_basic_lr = sum( log(1 + exp( (repmat(-y(1:nn_test,:), 1, d)...
-    .* transpose(A(:,1:nn_test)) )*x_ast)) );
-minimize( cumu_obj_basic_lr );
-cvx_end
-x_opt_basic = x_ast;
-
-%for beta1
-cvx_begin quiet
-variable x_ast(d,1)
-cumu_obj_basic_lr = beta1 * sum( log(1 + exp( (repmat(-y(1:nn_test,:), 1, d)...
-    .* transpose(A(:,1:nn_test)) )*x_ast)) ) + (1-beta1) * sum(transpose(Xi(:,1:nn_test))* x_ast);
-minimize( cumu_obj_basic_lr );
-cvx_end        
-x_opt_beta1 = x_ast;
-
-%for beta2
-cvx_begin quiet
-variable x_ast(d,1)
-cumu_obj_basic_lr = beta2 * sum( log(1 + exp( (repmat(-y(1:nn_test,:), 1, d)...
-    .* transpose(A(:,1:nn_test)) )*x_ast)) ) + (1-beta2) * sum(transpose(Xi(:,1:nn_test))* x_ast);
-minimize( cumu_obj_basic_lr );
-cvx_end        
-x_opt_beta2 = x_ast;
-
-%for beta3
-cvx_begin quiet
-variable x_ast(d,1)
-cumu_obj_basic_lr = beta3 * sum( log(1 + exp( (repmat(-y(1:nn_test,:), 1, d)...
-    .* transpose(A(:,1:nn_test)) )*x_ast)) ) + (1-beta3) * sum(transpose(Xi(:,1:nn_test))* x_ast);
-minimize( cumu_obj_basic_lr );
-cvx_end        
-x_opt_beta3 = x_ast;
 
 
 %construct the confusion matrix W. Ring topology 
@@ -88,28 +59,20 @@ end
 W = W/3;
 
 
-
-
-%online learning
-Q = zeros(d-1,d);
-for row = 1:d-1
-    Q(row,row) = 1; Q(row,row+1) = -1;
-end
-
-X_t_basic_lr = zeros(d,n);
-X_t_our_lr1 = zeros(d,n);
-X_t_our_lr2 = zeros(d,n);
-X_t_our_lr3 = zeros(d,n);
+X_t_basic_lr = ones(d,n);
+X_t_our_lr1 = ones(d,n);
+X_t_our_lr2 = ones(d,n);
+X_t_our_lr3 = ones(d,n);
 Grad_basic = zeros(d,n);
 Grad_our1 = zeros(d,n);
 Grad_our2 = zeros(d,n);
 Grad_our3 = zeros(d,n);
 
 
-Regret_basic_lr = zeros(1,n);
-Regret_our_lr1 = zeros(1,n);
-Regret_our_lr2 = zeros(1,n);
-Regret_our_lr3 = zeros(1,n);
+Regret_basic_lr = 0;
+Regret_our_lr1 = 0;
+Regret_our_lr2 = 0;
+Regret_our_lr3 = 0;
 Loss_basic_lr = zeros(1,n);
 Loss_basic_lr_opt = zeros(1,n);
 Loss_our_lr1 = zeros(1,n);
@@ -136,7 +99,7 @@ for t=1:T
         %A_it = randn(d,1);
         xi_it = Xi(:,(t-1)*n+i);
         grad_basic = (-y_it * A_it) / (1 + exp(y_it * A_it'* X_t_basic_lr(:,i))); %gradient - basic lr
-        grad_our_temp1 = (-y_it * A_it) / (1 + exp(y_it * A_it'* X_t_our_lr1(:,i)));
+        grad_our_temp1 = (-y_it * A_it) / (1 + exp(y_it * A_it'* X_t_our_lr1(:,i))) ;
         grad_our_temp2 = (-y_it * A_it) / (1 + exp(y_it * A_it'* X_t_our_lr2(:,i)));
         grad_our_temp3 = (-y_it * A_it) / (1 + exp(y_it * A_it'* X_t_our_lr3(:,i)));
         grad_h_t1 = xi_it;
@@ -150,36 +113,60 @@ for t=1:T
         Grad_our2(:,i) = grad_our2;
         Grad_our3(:,i) = grad_our3;
     end
-    X_t_basic_lr = X_t_basic_lr * W - eta * Grad_basic; %update rule - basic lr
-    X_t_our_lr1 = X_t_our_lr1 * W - eta * Grad_our1; %update rule - our lr
-    X_t_our_lr2 = X_t_our_lr2 * W - eta * Grad_our2; %update rule - our lr
-    X_t_our_lr3 = X_t_our_lr3 * W - eta * Grad_our3; %update rule - our lr
+    
+    X_t_basic_lr_opt = X_t_basic_lr;
+    X_t_our_lr1_opt = X_t_our_lr1;
+    X_t_our_lr2_opt = X_t_our_lr2;
+    X_t_our_lr3_opt = X_t_our_lr3;
+    
+    
+    X_t_basic_lr = X_t_basic_lr * W - eta/sqrt(t) * Grad_basic; %update rule - basic lr
+    X_t_our_lr1 = X_t_our_lr1 * W - eta/sqrt(t) * Grad_our1; %update rule - our lr
+    X_t_our_lr2 = X_t_our_lr2 * W - eta/sqrt(t) * Grad_our2; %update rule - our lr
+    X_t_our_lr3 = X_t_our_lr3 * W - eta/sqrt(t) * Grad_our3; %update rule - our lr
+    
+    
+    
+    X_t_basic_lr_opt = X_t_basic_lr_opt * W - eta_opt_basic/sqrt(t) * Grad_basic; %update rule - basic lr
+    X_t_our_lr1_opt = X_t_our_lr1_opt* W - eta_opt_1/sqrt(t) * Grad_our1;
+    X_t_our_lr2_opt = X_t_our_lr2_opt* W - eta_opt_2/sqrt(t) * Grad_our2;
+    X_t_our_lr3_opt = X_t_our_lr3_opt* W - eta_opt_3/sqrt(t) * Grad_our3;
+
+    
     
     for i=1:n
+        xi_it = Xi(:,(t-1)*n+i);
         %evaluate dynamic regret
         Loss_basic_lr(:,i) = Loss_basic_lr(:,i) + log(1 + exp(-y_it*A_it' * X_t_basic_lr(:,i)));
-        Loss_basic_lr_opt(:,i) = Loss_basic_lr_opt(:,i) + log(1 + exp(-y_it*A_it' * x_opt_basic));
+        Loss_basic_lr_opt(:,i) = Loss_basic_lr_opt(:,i) + log(1 + exp(-y_it*A_it' * X_t_basic_lr_opt(:,i)));
+        Regret_basic_lr = Regret_basic_lr + max(0,Loss_basic_lr(:,i)  - Loss_basic_lr_opt(:,i) );
+        
         Loss_our_lr1(:,i) = Loss_our_lr1(:,i) + beta1 * log(1 + exp(-y_it*A_it'*X_t_our_lr1(:,i))) + (1-beta1)...
             * (xi_it'*X_t_our_lr1(:,i));
-        Loss_our_lr1_opt(:,i) = Loss_our_lr1_opt(:,i) + beta1 * log(1 + exp(-y_it*A_it'*x_opt_beta1)) + (1-beta1)...
-            * (xi_it'*x_opt_beta1);
+        Loss_our_lr1_opt(:,i) = Loss_our_lr1_opt(:,i) + beta1 * log(1 + exp(-y_it*A_it'*X_t_our_lr1_opt(:,i))) + (1-beta1)...
+            * (xi_it'*X_t_our_lr1_opt(:,i));
+        Regret_our_lr1 = Regret_our_lr1 + max(0,Loss_our_lr1(:,i)-Loss_our_lr1_opt(:,i));
+        
         Loss_our_lr2(:,i) = Loss_our_lr2(:,i) + beta2 * log(1 + exp(-y_it*A_it'*X_t_our_lr2(:,i))) + (1-beta2)...
             * (xi_it'*X_t_our_lr2(:,i));
-        Loss_our_lr2_opt(:,i) = Loss_our_lr2_opt(:,i) + beta2 * log(1 + exp(-y_it*A_it'*x_opt_beta2)) + (1-beta2)...
-            * (xi_it'*x_opt_beta2);
+        Loss_our_lr2_opt(:,i) = Loss_our_lr2_opt(:,i) + beta2 * log(1 + exp(-y_it*A_it'*X_t_our_lr2_opt(:,i))) + (1-beta2)...
+            * (xi_it'*X_t_our_lr2_opt(:,i));
+        Regret_our_lr2 = Regret_our_lr2 + max(0,Loss_our_lr2(:,i)-Loss_our_lr2_opt(:,i));
+        
         Loss_our_lr3(:,i) = Loss_our_lr3(:,i) + beta3 * log(1 + exp(-y_it*A_it'*X_t_our_lr3(:,i))) + (1-beta3)...
             * (xi_it'*X_t_our_lr3(:,i));
-        Loss_our_lr3_opt(:,i) = Loss_our_lr3_opt(:,i) + beta3 * log(1 + exp(-y_it*A_it'*x_opt_beta3)) + (1-beta3)...
-            * (xi_it'*x_opt_beta3);
+        Loss_our_lr3_opt(:,i) = Loss_our_lr3_opt(:,i) + beta3 * log(1 + exp(-y_it*A_it'*X_t_our_lr3_opt(:,i))) + (1-beta3)...
+            * (xi_it'*X_t_our_lr3_opt(:,i));
+        Regret_our_lr3 = Regret_our_lr3 + max(0,Loss_our_lr3(:,i)-Loss_our_lr3_opt(:,i));
     end
     
     
-    if mod(t,1000) == 0
+    if mod(t,5000) == 0
         
-        output = ['time=' mat2str(round(toc,1)) ' | regret-basic=' mat2str(sum(max(0,Loss_basic_lr - Loss_basic_lr_opt)))...
-            ' | regret-our(beta1)=' mat2str(sum(max(0,Loss_our_lr1 - Loss_our_lr1_opt)))...
-            ' | regret-our(beta2)=' mat2str(sum(max(0,Loss_our_lr2 - Loss_our_lr2_opt)))...
-            ' | regret-our(beta3)=' mat2str(sum(max(0,Loss_our_lr3 - Loss_our_lr3_opt)))];
+        output = ['time=' mat2str(round(toc,1)) ' | regret-basic=' mat2str(Regret_basic_lr)...
+            ' | regret-our(beta1)=' mat2str(Regret_our_lr1)...
+            ' | regret-our(beta2)=' mat2str(Regret_our_lr2)...
+            ' | regret-our(beta3)=' mat2str(Regret_our_lr3)];
         fprintf([output '\n']);
 
         fid=fopen('./output.txt','a');
