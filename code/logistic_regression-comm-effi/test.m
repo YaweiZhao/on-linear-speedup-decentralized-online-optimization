3078f37b12e3318a10e4870e5ee21ddcc3805869
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2019-01-13 22:10:22 -0500

    d

--- code/logistic_regression-comm-effi/test.m
+++ code/logistic_regression-comm-effi/test.m
@@ -22,7 +22,7 @@ for i=1:nn/n
         if y((i-1)*n+j,:) == 1
             A(:,(i-1)*n+j) = 1+sin(i)/10 + randn(d,1);
         else
-            A(:,(i-1)*n+j) = -1+sin(i)/10 + randn(d,1);
+            A(:,(i-1)*n+j) = -1-sin(i)/10 + randn(d,1);
         end
         Xi(:,(i-1)*n+j) = normrnd(0, (cos(i)+1)/10,d,1);
     end
