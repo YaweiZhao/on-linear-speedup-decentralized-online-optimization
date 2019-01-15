2a6d3110bd83140771fb219ea5d0bf1045804e68
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2019-01-15 11:23:59 -0500

    Updates from ShareLaTeX

--- code/cvx/cvx_version.m
+++ code/cvx/cvx_version.m
@@ -14,7 +14,7 @@ args = varargin;
 compile = false;
 quick = nargout > 0;
 if nargin
-    if ~ischar( args{1} ),
+    if ~ischar( args{1} )
         quick = true;
     else
         tt = strcmp( args, '-quick' );
@@ -26,7 +26,7 @@ if nargin
     end
 end
 
-if isfield( cvx___, 'loaded' ),
+if isfield( cvx___, 'loaded' )
     
     if quick, return; end
     fs = cvx___.fs;
@@ -39,25 +39,25 @@ else
     isoctave = exist( 'OCTAVE_VERSION', 'builtin' );
 
     % File and path separators, MEX extension
-    if isoctave,
+    if isoctave
         comp = octave_config_info('canonical_host_type');
         mext = 'mex';
         izpc = false;
         izmac = false;
-        if octave_config_info('mac'),
+        if octave_config_info('mac')
             msub = 'mac';
             izmac = true;
-        elseif octave_config_info('windows'),
+        elseif octave_config_info('windows')
             msub = 'win';
             izpc = true;
-        elseif octave_config_info('unix') && any(strfind(comp,'linux')),
+        elseif octave_config_info('unix') && any(strfind(comp,'linux'))
             msub = 'lin';
         else
             msub = 'unknown';
         end
-        if ~isempty( msub ),
+        if ~isempty( msub )
             msub = [ 'o_', msub ];
-            if strncmp( comp, 'x86_64', 6 ),
+            if strncmp( comp, 'x86_64', 6 )
                 msub = [ msub, '64' ];
             else
                 msub = [ msub, '32' ];
@@ -70,7 +70,7 @@ else
         mext = mexext;
         msub = '';
     end
-    if izpc,
+    if izpc
         fs = '\'; 
         fsre = '\\';
         ps = ';'; 
@@ -93,7 +93,7 @@ else
     nver = sscanf(nver,'%d');
     nver = nver(1) + 0.01 * ( nver(2) + 0.01 * nver(3) );
     
-    if isoctave || ~usejava('jvm'),
+    if isoctave || ~usejava('jvm')
         jver = 0;
     else
         jver = char(java.lang.System.getProperty('java.version'));
@@ -123,8 +123,8 @@ end
 % Quick exit for non-verbose output %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-if quick,
-    if nargout,
+if quick
+    if nargout
         varargout = { fs, cvx___.ps, mpath, cvx___.mext };
     end
     cvx_load_prefs( false );
@@ -137,21 +137,21 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 cvx_ver = '2.1';
-cvx_bld = '1116';
-cvx_bdate = 'Thu Mar 30 21:54:11 2017';
-cvx_bcomm = 'd4cc5c5';
+cvx_bld = '1127';
+cvx_bdate = 'Sat Dec 15 18:52:07 2018';
+cvx_bcomm = '95903bf';
 line = '---------------------------------------------------------------------------';
 fprintf( '\n%s\n', line );
 fprintf( 'CVX: Software for Disciplined Convex Programming       (c)2014 CVX Research\n' );
 fprintf( 'Version %3s, Build %4s (%7s)%42s\n', cvx_ver, cvx_bld, cvx_bcomm, cvx_bdate );
 fprintf( '%s\n', line );
 fprintf( 'Installation info:\n    Path: %s\n', cvx___.where );
-if isoctave,
+if isoctave
     fprintf( '    GNU Octave %s on %s\n', version, cvx___.comp );
 else
     verd = ver('MATLAB');
     fprintf( '    MATLAB version: %s %s\n', verd.Version, verd.Release );
-    if usejava( 'jvm' ),
+    if usejava( 'jvm' )
         os_name = char(java.lang.System.getProperty('os.name'));
         os_arch = char(java.lang.System.getProperty('os.arch'));
         os_version = char(java.lang.System.getProperty('os.version'));
@@ -171,15 +171,15 @@ end
 issue = false;
 isoctave = cvx___.isoctave;
 nver = cvx___.nver;
-if isoctave,
-    if nver <= 3.08,
+if isoctave
+    if nver <= 3.08
         fprintf( '%s\nCVX is not yet supported on Octave.\n(Please do not waste further time trying: changes to Octave are required.\nBut they are coming! Stay tuned.)\n%s\n', line, line );
         issue = true;
     end
-elseif nver < 7.08 && strcmp( cvx___.comp(end-1:end), '64' ),
+elseif nver < 7.08 && strcmp( cvx___.comp(end-1:end), '64' )
     fprintf( '%s\nCVX requires MATLAB 7.8 or later (7.5 or later on 32-bit platforms).\n' , line, line );
     issue = true;
-elseif nver < 7.05,
+elseif nver < 7.05
     fprintf( '%s\nCVX requires MATLAB 7.5 or later (7.8 or later on 64-bit platforms).\n' , line, line );
     issue = true;
 end
@@ -189,26 +189,26 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%
 
 fid = fopen( [ mpath, fs, 'MANIFEST' ], 'r' );
-if fid > 0,
+if fid > 0
     fprintf( 'Verfying CVX directory contents:' );
     manifest = textscan( fid, '%s' );
     manifest = manifest{1};
     fclose( fid );
     newman = get_manifest( mpath, fs );
-    if ~isequal( manifest, newman ),
+    if ~isequal( manifest, newman )
         missing = setdiff( manifest, newman );
         additional = setdiff( newman, manifest );
-        if ~isempty( missing ) || ~isempty( additional ),
-            if fs ~= '/',
+        if ~isempty( missing ) || ~isempty( additional )
+            if fs ~= '/'
                 missing = strrep( missing, '/', fs );
                 additional = strrep( additional, '/', fs );
             end
-            if ~isempty( missing ),
+            if ~isempty( missing )
                 fprintf( '\n    WARNING: The following files/directories are missing:\n' );
                 isdir = cellfun(@(x)x(end)==fs,missing);
                 missing_d = missing(isdir);
                 missing_f = missing(~isdir);
-                while ~isempty( missing_d ),
+                while ~isempty( missing_d )
                     mdir = missing_d{1};
                     ss = strncmp( missing_d, mdir, length(mdir) );
                     tt = strncmp( missing_f, mdir, length(mdir) );
@@ -216,15 +216,15 @@ if fid > 0,
                     missing_d(ss) = [];
                     missing_f(tt) = [];
                 end
-                for k = 1 : min(length(missing_f),10),
+                for k = 1 : min(length(missing_f),10)
                     fprintf( '        %s%s%s\n', mpath, fs, missing_f{k} );
                 end
-                if length(missing_f) > 10,
+                if length(missing_f) > 10
                     fprintf( '        (and %d more files)\n', length(missing_f) - 10 );
                 end
                 fprintf( '    These omissions may prevent CVX from operating properly.\n'  );
             end
-            if ~isempty( additional ),
+            if ~isempty( additional )
                 if isempty( missing ), fprintf( '\n' ); end
                 fprintf( '    WARNING: The following extra files/directories were found:\n' );
                 isdir = cellfun(@(x)x(end)==fs,additional);
@@ -232,7 +232,7 @@ if fid > 0,
                 additional_d = additional(isdir&~issedumi);
                 additional_f = additional(~isdir&~issedumi);
                 additional_s = additional(issedumi);
-                while ~isempty( additional_d ),
+                while ~isempty( additional_d )
                     mdir = additional_d{1};
                     ss = strncmp( additional_d, mdir, length(mdir) );
                     tt = strncmp( additional_f, mdir, length(mdir) );
@@ -240,21 +240,21 @@ if fid > 0,
                     additional_d(ss) = [];
                     additional_f(tt) = [];
                 end
-                for k = 1 : min(length(additional_f),10),
+                for k = 1 : min(length(additional_f),10)
                     fprintf( '        %s%s%s\n', mpath, fs, additional_f{k} );
                 end
-                if length(additional_f) > 10,
+                if length(additional_f) > 10
                     fprintf( '        (and %d more files)\n', length(additional_f) - 10 );
                 end
                 fprintf( '    These files may alter the behavior of CVX in unsupported ways.\n' );
-                if ~isempty( additional_s ),
+                if ~isempty( additional_s )
                     fprintf( '    ERROR: obsolete versions of SeDuMi MEX files were found:\n' );
-                    for k = 1 : length(additional_s),
+                    for k = 1 : length(additional_s)
                         fprintf( '        %s%s%s\n', mpath, fs, additional_f{k} );
                     end
                     fprintf( '    These files are now obsolete, and must be removed to ensure\n' );
                     fprintf( '    that SeDuMi operates properly and produces sound results.\n' );
-                    if ~issue,
+                    if ~issue
                         fprintf( '    Please remove these files and re-run CVX_SETUP.\n' );
                         issue = true;
                     end
@@ -269,18 +269,18 @@ if fid > 0,
 else    
     fprintf( 'Manifest missing; cannot verify file structure.\n' ) ;
 end
-if ~compile,
+if ~compile
     mexpath = [ mpath, fs, 'lib', fs ];
     mext = cvx___.mext;
     if ( ~exist( [ mexpath, 'cvx_eliminate_mex.', mext ], 'file' ) || ...
-         ~exist( [ mexpath, 'cvx_bcompress_mex.', mext ], 'file' ) ) && ~issue,
+         ~exist( [ mexpath, 'cvx_bcompress_mex.', mext ], 'file' ) ) && ~issue
         issue = true;
-        if ~isempty( msub ),
+        if ~isempty( msub )
           mexpath = [ mexpath, msub, fs ];
           issue = ~exist( [ mexpath, 'cvx_eliminate_mex.mex' ], 'file' ) || ...
-                         ~exist( [ mexpath, 'cvx_bcompress_mex.mex' ], 'file' );
+                  ~exist( [ mexpath, 'cvx_bcompress_mex.mex' ], 'file' );
         end
-        if issue,
+        if issue
           fprintf( '    ERROR: one or more MEX files for this platform are missing.\n' );
           fprintf( '    These files end in the suffix ".%s". CVX will not operate\n', mext );
           fprintf( '    without these files. Please visit\n' );
@@ -300,27 +300,26 @@ cvx_load_prefs( true );
 % License file %
 %%%%%%%%%%%%%%%%
 
-if isoctave,
-    if ~isempty( cvx___.license ),
-        fprintf( 'CVX Professional is not supported with Octave.\n' );
+if any( exist( 'cvx_license', 'file' ) == [ 2, 3, 6 ] )
+    if isoctave
+        if ~isempty( cvx___.license )
+            fprintf( 'CVX Professional is not supported with Octave.\n' );
+        end
+    else
+        cvx_license( args{:} );
     end
-elseif cvx___.jver < 1.6,
-    fprintf('       WARNING: full support for CVX Professional licenses\n' );
-    fprintf('       requres Java version 1.6.0 or later. Please upgrade.\n' );
-elseif exist( 'cvx_license', 'file' ),
-    cvx_license( args{:} );
 end
 
 %%%%%%%%%%%%%%%
 % Wrapping up %
 %%%%%%%%%%%%%%%
 
-if ~issue,
+if ~issue
     cvx___.loaded = true;
 end
 clear fs;
 fprintf( '%s\n', line );
-if length(dbstack) <= 1,
+if length(dbstack) <= 1
     fprintf( '\n' );
 end
 
@@ -334,10 +333,10 @@ global cvx___
 fs = cvx___.fs;
 isoctave = cvx___.isoctave;
 errmsg = '';
-if verbose,
+if verbose
     fprintf( 'Preferences: ' );
 end
-if isoctave,
+if isoctave
     pfile = [ prefdir, fs, '.cvx_prefs.mat' ];
 else
     pfile = [ regexprep( prefdir(1), [ cvx___.fsre, 'R\d\d\d\d\w$' ], '' ), fs, 'cvx_prefs.mat' ];
@@ -347,9 +346,9 @@ try
     if exist( pfile, 'file' )
         outp = load( pfile );
         pfile2 = pfile;
-    elseif ~isoctave,
+    elseif ~isoctave
         pfile2 = [ prefdir, fs, 'cvx_prefs.mat' ];
-        if exist( pfile2, 'file' ),
+        if exist( pfile2, 'file' )
             outp = load( pfile2 );
         end
     end
@@ -357,7 +356,7 @@ catch errmsg
     errmsg = cvx_error( errmsg, 67, false, '    ' );
     errmsg = sprintf( 'CVX encountered the following error attempting to load your preferences:\n%sPlease attempt to diagnose this error and try again.\nYou may need to re-run CVX_SETUP as well.\nIn the meanwhile, preferences will be set to their defaults.\n', errmsg );
 end
-if ~isempty( outp ),
+if ~isempty( outp )
     try
         cvx___.expert = outp.expert;
         cvx___.precision = outp.precision;
@@ -371,7 +370,7 @@ if ~isempty( outp ),
         errmsg = 'Your CVX preferences file seems out of date; default preferences will be used.';
     end
 end
-if isempty( outp ),
+if isempty( outp )
     cvx___.expert = false;
     cvx___.precision = [eps^0.5,eps^0.5,eps^0.25];
     cvx___.precflag = 'default';
@@ -381,15 +380,15 @@ if isempty( outp ),
     cvx___.license = [];
 end
 cvx___.pfile = pfile;
-if verbose,
-    if ~isempty( errmsg ),
+if verbose
+    if ~isempty( errmsg )
         fprintf( 'error during load:\n%s', cvx_error( errmsg, 70, false, '   ' ) );
-    elseif isempty( cvx___.path ),
+    elseif isempty( cvx___.path )
         fprintf( 'none found; defaults loaded.\n' );
     else
         fprintf( '\n    Path: %s\n', pfile2 );
     end
-elseif ~isempty( errmsg ),
+elseif ~isempty( errmsg )
     warning( 'CVX:BadPrefsLoad', errmsg );
 end
 
@@ -405,18 +404,18 @@ ndir   = '';
 dndx   = 0;
 pat2   = '^\.|~$|';
 pat    = '^\.|~$|^cvx_license.[md]at$|^doc$|^examples$';
-while true,
+while true
     isdir  = [ nfiles.isdir ];
     nfiles = { nfiles.name };
     tt     = cellfun( @isempty, regexp( nfiles, pat ) ); pat = pat2;
     isdir  = isdir(tt);
     nfiles = nfiles(tt);
     ndirs  = nfiles(isdir);
-    if ~isempty(ndirs),
+    if ~isempty(ndirs)
         dirs = horzcat( dirs, strcat(strcat(ndir,ndirs), fs ) ); %#ok
     end
     nfiles = nfiles(~isdir);
-    if ~isempty(nfiles),
+    if ~isempty(nfiles)
         files = horzcat( files, strcat(ndir,nfiles) ); %#ok
     end
     if length( dirs ) == dndx, break; end
@@ -427,7 +426,7 @@ end
 [tmp,ndxs1] = sort(upper(dirs)); %#ok
 [tmp,ndxs2] = sort(upper(files)); %#ok
 newman = horzcat( dirs(ndxs1), files(ndxs2) );
-if fs ~= '/',
+if fs ~= '/'
     newman = strrep( newman, fs, '/' );
 end
 newman = newman(:);
--- code/logistic_regression-comm-effi/test.m
+++ code/logistic_regression-comm-effi/test.m
@@ -1,45 +1,77 @@
-
-%load data
-fn = './spam.mat';
-data = load(fn);
-data = data.new_data;
-[nn,d] = size(data);
-A = transpose(data(:,2:d));
-y = data(:,1);
-d = d-1;
+clc;
+clear all;
+rng('default');
+d = 10;
+n = 100; % # of nodes
+nn = 4000*n;
 
 %hyper-parameter
-T=800;
-eta = 1e-2;
-lambda = 1e-1;
-beta1 = 0.9;%varying beta1 0.9 0.8 0.7
-beta2 = 0.8;
-beta3 = 0.7;
+beta1 = 0.1;
+
 M = 10; %dynamics
-n = 5; % # of nodes
+eta = sqrt(M);
+gamma= 1e-3;
+%hyper-parameter
+T=nn/n;
+
+A = zeros(d,nn);
+y = sign(rand(nn,1)-0.5);
+for i=1:T
+    for j=1:n
+        temp1 = rand(d,1)-0.5;
+        temp2 = 1+sin(i)/2 + randn(d,1);
+        temp3 = -1+sin(i)/2 + randn(d,1);
+        if y((i-1)*n+j,:) == 1
+            A(:,(i-1)*n+j) = beta1*temp1 + (1-beta1)*temp2;
+        else
+            A(:,(i-1)*n+j) = beta1*temp1 + (1-beta1)*temp3;
+        end
+        
+    end
+    
+    
+end
+
 
 %construct the confusion matrix W. Ring topology 
-W =  eye(n);
-for i=1:n
-    for j=1:n
-        if i==n
-             W(i,1) = 1;
-        end
-        if i+1 <= n && j == i+1
-            W(i,j) = 1;
-        end
-        if i==1
-            W(i,n) = 1;
-        end
-        if i-1>=1 && j == i-1
-            W(i,j) = 1;
+tag = 'decentralized';
+topology = 'wattsStrog';
+if strcmp(tag, 'centralized')
+    W =  ones(n,n)/n;
+elseif strcmp(tag, 'decentralized') && strcmp(topology, 'wattsStrog')
+    graph = WattsStrogatz(n,3,1);
+    edges_list = graph.Edges.EndNodes;
+    [n_edges,~] = size(edges_list);
+    W = eye(n);
+    for i=1:n_edges
+        W(edges_list(i,1), edges_list(i,2)) = 1;
+        W(edges_list(i,2), edges_list(i,1)) = 1;
+    end
+    for i=1:n
+        W(i,:) = W(i,:)/sum(W(i,:));
+    end
+elseif strcmp(tag, 'decentralized') && strcmp(topology, 'guli')
+    W = eye(n);
+elseif strcmp(tag, 'decentralized') && strcmp(topology, 'ring')
+    W =  eye(n);
+    for i=1:n
+        for j=1:n
+            if i==n
+                 W(i,1) = 1;
+            end
+            if i+1 <= n && j == i+1
+                W(i,j) = 1;
+            end
+            if i==1
+                W(i,n) = 1;
+            end
+            if i-1>=1 && j == i-1
+                W(i,j) = 1;
+            end
         end
     end
+    W = W/3;
 end
-W = W/3;
-
-
-
 
 %online learning
 Q = zeros(d-1,d);
@@ -48,153 +80,74 @@ for row = 1:d-1
 end
 
 X_t_basic_lr = zeros(d,n);
-X_t_our_lr1 = zeros(d,n);
-X_t_our_lr2 = zeros(d,n);
-X_t_our_lr3 = zeros(d,n);
 Grad_basic = zeros(d,n);
-Grad_our1 = zeros(d,n);
-Grad_our2 = zeros(d,n);
-Grad_our3 = zeros(d,n);
-
-
 Regret_basic_lr = zeros(1,n);
-Regret_our_lr1 = zeros(1,n);
-Regret_our_lr2 = zeros(1,n);
-Regret_our_lr3 = zeros(1,n);
 Loss_basic_lr = zeros(1,n);
-Loss_our_lr1 = zeros(1,n);
-Loss_our_lr2 = zeros(1,n);
-Loss_our_lr3 = zeros(1,n);
+Cumu_Loss_basic_lr = zeros(1,n);
 
 loss_basic_lr = 0;
-loss_our_lr1 = 0;
-loss_our_lr2 = 0;
-loss_our_lr3 = 0;
+
 tic;
+cumu_loss_draw = [];
+loss_draw = [];
 for t=1:T
-    
+
     for i=1:n % # of nodes
+        
         y_it = y((t-1)*n+i,:);
         A_it = A(:,(t-1)*n+i);
-
-        grad_basic = (-y_it * A_it) / (1 + exp(y_it * A_it'* X_t_basic_lr(:,i))); %gradient - basic lr
-        grad_our_temp1 = (-y_it * A_it) / (1 + exp(y_it * A_it'* X_t_our_lr1(:,i)));
-        grad_our_temp2 = (-y_it * A_it) / (1 + exp(y_it * A_it'* X_t_our_lr2(:,i)));
-        grad_our_temp3 = (-y_it * A_it) / (1 + exp(y_it * A_it'* X_t_our_lr2(:,i)));
-        grad_h_t1 = lambda * Q' * sign(Q*X_t_our_lr1(:,i));
-        grad_h_t2 = lambda * Q' * sign(Q*X_t_our_lr2(:,i));
-        grad_h_t3 = lambda * Q' * sign(Q*X_t_our_lr3(:,i));
-        grad_our1 = beta1 * grad_our_temp1 + (1-beta1) * grad_h_t1; %gradient - our lr
-        grad_our2 = beta2 * grad_our_temp2 + (1-beta2) * grad_h_t2; %gradient - our lr
-        grad_our3 = beta1 * grad_our_temp3 + (1-beta3) * grad_h_t3; %gradient - our lr
-        Grad_basic(:,i) =  grad_basic;
-        Grad_our1(:,i) = grad_our1;
-        Grad_our2(:,i) = grad_our2;
-        Grad_our3(:,i) = grad_our3;
-    end
-    X_t_basic_lr = X_t_basic_lr * W - eta * Grad_basic; %update rule - basic lr
-    X_t_our_lr1 = X_t_our_lr1 * W - eta * Grad_our1; %update rule - our lr
-    X_t_our_lr2 = X_t_our_lr2 * W - eta * Grad_our2; %update rule - our lr
-    X_t_our_lr3 = X_t_our_lr3 * W - eta * Grad_our3; %update rule - our lr
-    
-    for i=1:n
-        %evaluate dynamic regret
-        Loss_basic_lr(:,i) = Loss_basic_lr(:,i) + log(1 + exp(-y_it*A_it' * X_t_basic_lr(:,i)));
-        Loss_our_lr1(:,i) = Loss_our_lr1(:,i) + beta1 * log(1 + exp(-y_it*A_it'*X_t_our_lr1(:,i))) + (1-beta1)...
-            * lambda * norm(Q*X_t_our_lr1(:,i), 2);
-        Loss_our_lr2(:,i) = Loss_our_lr2(:,i) + beta2 * log(1 + exp(-y_it*A_it'*X_t_our_lr2(:,i))) + (1-beta2)...
-            * lambda * norm(Q*X_t_our_lr2(:,i), 2);
-        Loss_our_lr3(:,i) = Loss_our_lr3(:,i) + beta3 * log(1 + exp(-y_it*A_it'*X_t_our_lr3(:,i))) + (1-beta3)...
-            * lambda * norm(Q*X_t_our_lr3(:,i), 2);
+        grad_basic = (-y_it * A_it) / (1 + exp(y_it * A_it'* X_t_basic_lr(:,i)))...
+            + gamma*X_t_basic_lr(:,i); %gradient - basic lr
         
+        Grad_basic(:,i) =  grad_basic;
+
+    end
+    X_t_basic_lr = X_t_basic_lr * W - eta/sqrt(t) * Grad_basic; %update rule - basic lr
+   
+    for i=1:n
+        temp1 = log(1 + exp(-y((t-1)*n+i,:)*transpose(A(:,(t-1)*n+i))* X_t_basic_lr(:,i)));
+        temp2 = gamma/2*(transpose(X_t_basic_lr(:,i))*X_t_basic_lr(:,i));
+        Loss_basic_lr(:,i) =  temp1 + temp2;
+    end
+    Cumu_Loss_basic_lr = Cumu_Loss_basic_lr + Loss_basic_lr;
+    %evaluate dynamic regret on the first node
+    if mod(t,fix(T/20)) == 0
+
         %auxiliary matrix R
         R = zeros(t-1,t);
         for row = 1:t-1
             R(row, row) = 1;
             R(row, row+1) = -1;
         end
-        
+
         % advantage = basic regre - basic lr
-        
-        cvx_begin quiet
-        variable x_ast(d,t)
-        cumu_obj_basic_lr = trace( log(1 + exp( (repmat(-y(i:n:(t-1)*n+i,:), 1, d)...
-            .* transpose(A(:,i:n:(t-1)*n+i)) )*x_ast)) );
-        minimize( cumu_obj_basic_lr );
-        subject to
-        norms( R * x_ast', 2 , 2) <= M; %dynamics M
-        cvx_end
-        Regret_basic_lr(:,i) = Loss_basic_lr(:,i) - cumu_obj_basic_lr;
-        
-        
-        
-        
-        cvx_begin quiet
-        variable x_ast(d,t)
-        cumu_obj_our_lr = trace( beta1 * log(1 + exp( repmat(-y(i:n:(t-1)*n+i,:), 1, d)...
-            .* transpose(A(:,i:n:(t-1)*n+i)) *x_ast)))...
-            +(1-beta1) * sum(lambda * norms(Q*x_ast,2,1) );
-        minimize( cumu_obj_our_lr );
-        subject to
-        norms( R * x_ast', 2 , 2) <= M; %dynamics M
-        cvx_end
-        Regret_our_lr1(:,i) = Loss_our_lr1(:,i) - cumu_obj_our_lr;
-        
-        cvx_begin quiet
-        variable x_ast(d,t)
-        cumu_obj_our_lr = trace( beta2 * log(1 + exp( repmat(-y(i:n:(t-1)*n+i,:), 1, d)...
-            .* transpose(A(:,i:n:(t-1)*n+i)) *x_ast)))...
-            +(1-beta2) * sum(lambda * norms(Q*x_ast,2,1) );
-        minimize( cumu_obj_our_lr );
-        subject to
-        norms( R * x_ast', 2 , 2) <= M; %dynamics M
-        cvx_end
-        Regret_our_lr2(:,i) = Loss_our_lr2(:,i) - cumu_obj_our_lr;
-        
-        cvx_begin quiet
-        variable x_ast(d,t)
-        cumu_obj_our_lr = trace( beta3 * log(1 + exp( repmat(-y(i:n:(t-1)*n+i,:), 1, d)...
-            .* transpose(A(:,i:n:(t-1)*n+i)) *x_ast)))...
-            +(1-beta3) * sum(lambda * norms(Q*x_ast,2,1) );
-        minimize( cumu_obj_our_lr );
-        subject to
-        norms( R * x_ast', 2 , 2) <= M; %dynamics M
-        cvx_end
-        Regret_our_lr3(:,i) = Loss_our_lr3(:,i) - cumu_obj_our_lr;
 
+%         cvx_begin quiet
+%         variable x_ast_basic(d,t)
+%         cumu_obj_basic_lr = log(1 + exp(ones(1,d) * (transpose(-y(1:n:(t-1)*n+1,:)*ones(1,d))...
+%             .* A(:,1:n:(t-1)*n+1) .* x_ast_basic)))*ones(t,1)...
+%             +gamma/2*(ones(1,d)*(x_ast_basic .* x_ast_basic)*ones(t,1));
+%         minimize( cumu_obj_basic_lr );
+%         subject to
+%         ones(1,t-1)*norms( R * x_ast_basic', 2 , 2) <= M; %dynamics M
+%         cvx_end       
+ %       Regret_basic_lr(:,1) = Loss_basic_lr(:,1) - cumu_obj_basic_lr;
+%         fprintf('#rounds=%d | regret=%.2f | loss=%.2f\n',t,sum(Regret_basic_lr),sum(Loss_basic_lr));
+%         output = ['#rounds=' mat2str(t) ' | regret=' mat2str(round(sum(Regret_basic_lr),2))...
+%             'loss=' mat2str(round(sum(Loss_basic_lr),2)) '\n'];
+         loss_draw = [loss_draw sum(Loss_basic_lr)];
+         cumu_loss_draw = [cumu_loss_draw sum(Cumu_Loss_basic_lr)];
+%         fprintf('#rounds=%d |  loss=%.3f | cumu loss = %.3f\n',t,sum(Loss_basic_lr),sum(Cumu_Loss_basic_lr));
+%          output = [ mat2str(round(sum(Loss_basic_lr),3)) ' '];
+%         fid=fopen('./output.txt');
+%         fprintf(fid,'%s\n',output);
+%         fclose(fid);
 
-        
-    end
-    
-    output = ['time=' mat2str(round(toc,1)) ' | regret-basic=' mat2str(round(sum(Regret_basic_lr),2))...
-        ' | regret-our(beta1)=' mat2str(round(sum(Regret_our_lr1),2))...
-        ' | regret-our(beta2)=' mat2str(round(sum(Regret_our_lr2),2))...
-        ' | regret-our(beta3)=' mat2str(round(sum(Regret_our_lr3),2))];
-    fprintf([output '\n']);
-    
-    fid=fopen('./output.txt','a');
-    fprintf(fid,'%s\n',output);
-    fclose(fid);
-    
-    
-    
-    
-    
-    
-    
-    
+    end   
     
 end
 
-
-
-
-
-
-
-
-%useful functions
-
-
+fprintf(['loss>> ' mat2str(round(loss_draw,1)) ' \n'...
+    'cumu loss>> ' mat2str(round(cumu_loss_draw))]);
 
 
